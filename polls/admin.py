7237c43d65bd9a0667d9250ce9e537713e5c7aab
Eugene Kravchenko <evgeniy.kravchenko@oracle.com>
2017-09-20 13:13:11 +0300

    change admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,23 @@
+import os
+
+from django import forms
 from django.contrib import admin
 
+
 # Register your models here.
+
+
 from polls.models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    change_form_template = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates', 'admin', 'new_change_form.html'))
+    new_flag = False
+    def change_view(self, request, object_id, extra_context=None):
+        extra_context = extra_context or {}
+        if Question.objects.get(id=object_id).state != 'new':
+            extra_context['readonly'] = True
+        return super(QuestionAdmin, self).change_view(request, object_id, extra_context=extra_context)
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -1,26 +1,21 @@
 from django.db import models
 
+NEW = 'new'
+INP = 'inp'
+CLOSED = 'closed'
+STATE_CHOICES = (
+    (NEW, 'new question'),
+    (INP, 'in progress'),
+    (CLOSED, 'closed question'),
+)
 
-class State(models.Model):
-    NEW = 'new'
-    INP = 'inp'
-    CLOSED = 'closed'
-    STATE_CHOICES = (
-        (NEW, 'new question'),
-        (INP, 'in progress'),
-        (CLOSED, 'closed question'),
-    )
-    question_state = models.CharField(max_length=10, choices=STATE_CHOICES)
-    #
-    # def create_statuses(self, state):
-    #     self.create(self, state=state)
-    #
-    # for state in STATE_CHOICES:
-    #     create_statuses(state[1])
+
+# class State(models.Model):
+#     question_state = models.CharField(max_length=10, choices=STATE_CHOICES)
 
 
 class Question(models.Model):
-    state = models.ForeignKey(State, on_delete=models.CASCADE, default=0)
+    state = models.CharField(max_length=10, choices=STATE_CHOICES, default=NEW)
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
