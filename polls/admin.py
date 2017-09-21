21352ff1e58d3a32b73adc288ea5ff94d32f6206
Eugene Kravchenko <evgeniy.kravchenko@oracle.com>
2017-09-21 16:21:46 +0300

    tutorial03

--- polls/admin.py
+++ polls/admin.py
@@ -2,22 +2,29 @@ import os
 
 from django import forms
 from django.contrib import admin
-
+# from polls.forms import ReadOnlyAdminFields
 
 # Register your models here.
 
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 class QuestionAdmin(admin.ModelAdmin):
     change_form_template = os.path.abspath(os.path.join(os.path.dirname(__file__), 'templates', 'admin', 'new_change_form.html'))
-    new_flag = False
+
     def change_view(self, request, object_id, extra_context=None):
         extra_context = extra_context or {}
+
         if Question.objects.get(id=object_id).state != 'new':
-            extra_context['readonly'] = True
+            extra_context['has_delete_permission'] = False
+            extra_context['show_delete'] = False
         return super(QuestionAdmin, self).change_view(request, object_id, extra_context=extra_context)
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj.state != 'new':
+            return ('question_text', 'pub_date',)
+        else:
+            return super(QuestionAdmin, self).get_readonly_fields(request, obj)
 
 admin.site.register(Question, QuestionAdmin)
